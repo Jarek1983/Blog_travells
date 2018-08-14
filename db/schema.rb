24109450a60ac64812899c4fce76bad3f99004a3
Jarosław Żmijewski <jzmija@wp.pl>
2018-08-14 08:57:38 +0200

    Authorization ans Sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_101747) do
+ActiveRecord::Schema.define(version: 2018_08_14_060846) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2018_08_13_101747) do
     t.string "l_name"
     t.string "email"
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "userid"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
