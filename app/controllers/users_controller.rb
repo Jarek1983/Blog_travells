9bbfa6dc9bd0965dd627cbdb18403d863c26c27f
Jarosław Żmijewski <jzmija@wp.pl>
2018-08-14 10:11:04 +0200

    Created sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to articles_path, notice:
 			"welcome #{@user.userid}! You successfully created an account." 
 		else
