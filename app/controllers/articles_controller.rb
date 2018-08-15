f5e802e7d0a502d2e1c48d5391a971dad858f6d0
Jarosław Żmijewski <jzmija@wp.pl>
2018-08-15 12:12:54 +0200

    Selection - countries, avatar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
 before_action :admin_authorize, :except => [:index, :show, :search]
 
 	def index
-		@articles = Article.all
+		@articles = Article.all.reverse
 	end
 
 	def new
