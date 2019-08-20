18011d51ba41007b6aa09b0e11b0afdeb1b2a960
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-08-20 23:28:56 +0200

    Update

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -2,20 +2,9 @@ require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Blog"
-  end
-
-  test "should get root" do
-    get root_path
-    assert_response :success
-    assert_select "title", "#{@base_title}"
-  end
-
-  test "should get index" do
+  test "should get resource articles" do
     get articles_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
   end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get index" do
-    get users_path
-    assert_response :success
-  end
 
-  test "should get new" do
-    get new_user_path
-    assert_response :success
-  end
 
 end
